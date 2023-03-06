0228ba2d51c0c08db16f85fc5076cfad48e18bc7
eldson <eldson.caldasw@gmail.com>
2023-03-06 20:50:52 -0300

    form-add-book

--- src/assets/styles/modal-add-book.ts
+++ src/assets/styles/modal-add-book.ts
@@ -18,22 +18,117 @@ export const ModalAddBookStyle = styled.div `
     animation: animationModalAddBook .5s;
 
     .conatiner-form {
-        width: 50%;
-        height: 80%;
+        width: 600px;
+        height: 400px;
 
         z-index: 2000;
 
         background: white;
         border-radius: 7px;
 
+        display: flex;
+        flex-direction: column;
+
+        ::before {
+            content: "";
+            display: flex;
+
+            position: absolute;
+
+            width: 254px;
+            height: 400px;
+
+            background: #41B3B3;
+            border-radius: 7px 0px 0px 7px;
+        }
+
         header {
             padding: 15px 23px;
+            width: calc(100% - 46px);
+
             display: flex;
-            flex-direction: row-reverse;
+            flex-direction: row;
+            justify-content: space-between;
 
             .btn-close {
                 cursor: pointer;
             }
+        }
+
+        form {
+            padding: 0px 23px;
+            width: calc(100% - 46px);
+            height: 90%;
+
+            z-index: 1000;
+
+            display: flex;
+            /* justify-content: space-between; */
+            gap: 65px;
+
+            .preview-image-cover {
+                width: auto;
+                input {
+                    display: none;
+                }
+    
+                label {
+                    width: 205px;
+                    height: 307px;
+    
+                    display: flex;
+                    flex-direction: column;
+                    justify-content: center;
+                    align-items: center;
+
+                    z-index: 2500;
+    
+                    gap: 15px;
+    
+                    /* border: 2px dashed #BABABA; */
+    
+                    cursor: pointer;
+    
+                    span {
+                        font-size: 12px;
+                        font-weight: 700;
+                        color: #ffffff;
+                    }
+                }
+            }
+
+            .data-input-book {
+                width: auto;
+                display: flex;
+                flex-direction: column;
+                gap: 15px;
+
+                input {
+                    padding-left: 15px;
+                    width: 250px;
+                    height: 40px;
+                    border-radius: 7px;
+                    border: 1px solid grey;
+
+                    outline-color: #41B3B3;
+                    outline-width: 1px;
+                }
+
+                button {
+                    height: 40px;
+                    border: none;
+                    outline: none;
+
+                    background-color: #41B3B3;
+                    border-radius: 7px;
+
+                    color: white;
+                    font-weight: 700;
+
+                    cursor: pointer;
+                }
+            }
+
         }
     }
 
--- src/shared/components/modal-add-book.tsx
+++ src/shared/components/modal-add-book.tsx
@@ -2,6 +2,7 @@ import { ModalAddBookStyle } from "../../assets/styles/modal-add-book";
 import { useDispatch } from "react-redux";
 import { hideModalAddBook } from "../../redux/features/modalAddBook";
 import { CloseIcon } from "../../assets/icons/close";
+import { ImageIcon } from "../../assets/icons/image";
 
 export function ModalAddBook() {
     const dispatch = useDispatch()
@@ -13,10 +14,29 @@ export function ModalAddBook() {
         <ModalAddBookStyle>
             <div className="conatiner-form">
                 <header>
-                    <div className="btn-close" onClick={() => hideModal()}>
+                    <span>New book</span>
+                    <div className="btn-close" onClick={() => hideModal()} title="Close">
                         <CloseIcon/>
                     </div>
                 </header>
+                <form>
+                    <div className="preview-image-cover">
+                        <label htmlFor="image">
+                            <ImageIcon/>
+                            <span>Insert cover</span>
+                        </label>
+                        <input type="file" id="image"/>
+                    </div>
+                    <div className="data-input-book">
+                        <input type="text" placeholder="Name"/>
+                        <input type="text" placeholder="Author"/>
+                        <input type="text" placeholder="Gender"/>
+                        <input type="number" placeholder="Year"/>
+                        <input type="number" placeholder="Pages"/>
+
+                        <button>Add book</button>
+                    </div>
+                </form>
             </div>
         </ModalAddBookStyle>
     )
